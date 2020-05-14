818d0ce360d786e8cbf1ee86bc4c2529ae205e75
Eros Matip <eros.matip@gmail.com>
2020-05-14 19:54:24 +0200

    initialistaion room_delete

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
-const formidableMiddleware = require ("express-formidable");
+const formidableMiddleware = require("express-formidable");
 const mongoose = require("mongoose");
 
 const app = express();
-
+app.use(formidableMiddleware());
 mongoose.connect(process.env.MONGODB_URI);
 
-app.use(formidableMiddleware());
-
-
 app.all("*", (req, res) => {
-    res.status(404).json({message: "Bad URL"});
-})
-
+    res.status(404).json({ message: "Bad URL" });
+});
+// *********** SERVER ***********
 app.listen(process.env.PORT, () => {
     console.log("Server started");
-  });
+});
