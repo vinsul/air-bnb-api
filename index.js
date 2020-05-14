30752c978f7fcf9eef629b2d9afb290b011dd536
Eros Matip <eros.matip@gmail.com>
2020-05-14 15:08:06 +0200

    sign_up test before push

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
-const formidableMiddleware = require ("express-formidable");
+const formidableMiddleware = require("express-formidable");
 const mongoose = require("mongoose");
 const signUpRoute = require("./routes/sign_up");
 
 const app = express();
 
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 app.use(formidableMiddleware());
 
 app.use(signUpRoute);
 
 app.all("*", (req, res) => {
-    res.status(404).json({message: "Bad URL"});
-})
+    res.status(404).json({ message: "Bad URL" });
+});
 
 app.listen(process.env.PORT, () => {
     console.log("Server started");
-  });
+});
