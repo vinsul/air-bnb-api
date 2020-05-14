308a63ea1b498ac90c3996537645d8e67c959bae
Eros Matip <eros.matip@gmail.com>
2020-05-14 17:38:20 +0200

    adding log_in by password

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
-const formidableMiddleware = require ("express-formidable");
+const formidableMiddleware = require("express-formidable");
 const mongoose = require("mongoose");
-const logInRoute = require("./routes/log_in");
-const signUpRoute = require("./routes/sign_up");
-
 const app = express();
-
-mongoose.connect(process.env.MONGODB_URI);
-
 app.use(formidableMiddleware());
 
-app.use(logInRoute);
-app.use(signUpRoute);
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+// *********** ROUTE ***********
+
+const log_in = require("./routes/log_in");
+app.use(log_in);
 
 app.all("*", (req, res) => {
-    res.status(404).json({message: "Bad URL"});
-})
-
+    res.status(404).json({ message: "Bad URL" });
+});
+// *********** SERVER ***********
 app.listen(process.env.PORT, () => {
     console.log("Server started");
-  });
+});
