8ed68e6838fafbca4eee7f1af4a753fdddd35927
Eros Matip <eros.matip@gmail.com>
2020-05-16 16:30:23 +0200

    filter's page created

--- routes/get_room.js
+++ routes/get_room.js
@@ -4,39 +4,41 @@ const Picture = require("../models/Picture");
 
 const router = express.Router();
 
-router.get("/room", async (req, res) => {
+router.get("/room", async(req, res) => {
     try {
-
         const filter = {};
-        if(req.query.id){
+
+        if (req.query.id) {
             filter._id = req.query.id;
         }
+
         const room_to_get = await Room.findOne(filter)
-        .populate({path: "creator", select: "username profile_picture"})
-        .populate("room_picture");
-        if(!room_to_get){
-            return res.status(400).json({message: "No room to display"});
+            .populate({ path: "creator", select: "username profile_picture" })
+            .populate("room_picture");
+        if (!room_to_get) {
+            return res.status(400).json({ message: "No room to display" });
         }
+
         const profile_picture_displayed = {};
         const room_picture_displayed = [];
 
-        for (let i = 0; i < room_to_get.room_picture.length; i++){
-            if(room_to_get.room_picture[i].is_active){
+        for (let i = 0; i < room_to_get.room_picture.length; i++) {
+            if (room_to_get.room_picture[i].is_active) {
                 room_picture_displayed.push({
                     id: room_to_get.room_picture[i]._id,
-                    secure_url: room_to_get.room_picture[i].infos.secure_url
+                    secure_url: room_to_get.room_picture[i].infos.secure_url,
                 });
             }
         }
 
-        if(room_to_get.creator.profile_picture){
+        if (room_to_get.creator.profile_picture) {
             const pic = await Picture.findById(room_to_get.creator.profile_picture);
-            if(pic.is_active){
+            if (pic.is_active) {
                 profile_picture_displayed.id = room_to_get.creator.profile_picture;
                 profile_picture_displayed.secure_url = pic.infos.secure_url;
             }
         }
-        
+
         res.status(200).json({
             id: room_to_get._id,
             title: room_to_get.title,
@@ -46,11 +48,11 @@ router.get("/room", async (req, res) => {
             room_picture: room_picture_displayed,
             creator: {
                 username: room_to_get.creator.username,
-                profile_picture: profile_picture_displayed
-            }
+                profile_picture: profile_picture_displayed,
+            },
         });
     } catch (error) {
-        res.status(400).json({message: error.message});
+        res.status(400).json({ message: error.message });
     }
 });
 
